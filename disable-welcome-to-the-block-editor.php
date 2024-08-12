b527228740b3c57c86b32788465e9a106d7d4f1d
WPExplorer <wpexplorer@gmail.com>
2024-08-12 15:00:50 -0700

    Update disable-welcome-to-the-block-editor.php

--- disable-welcome-to-the-block-editor.php
+++ disable-welcome-to-the-block-editor.php
@@ -8,17 +8,17 @@ defined( 'ABSPATH' ) || exit;
 
 if ( is_admin() ) {
 
-  function disable_welcome_to_the_block_editor() {
-  	?>
-  	<script>
-  		window.onload = (event) => {
-  			wp.data && wp.data.select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) && wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
-  		};
-  	</script>
-  	<?php
-  }
-  add_action( 'admin_head-post.php', 'disable_welcome_to_the_block_editor' );
-  add_action( 'admin_head-post-new.php',  'disable_welcome_to_the_block_editor' );
-  add_action( 'admin_head-edit.php', 'disable_welcome_to_the_block_editor' );
+	function disable_welcome_to_the_block_editor() {
+		?>
+		<script>
+			window.onload = (event) => {
+				wp.data && wp.data.select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) && wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
+			};
+		</script>
+	<?php
+	}
+	add_action( 'admin_head-post.php', 'disable_welcome_to_the_block_editor' );
+	add_action( 'admin_head-post-new.php',  'disable_welcome_to_the_block_editor' );
+	add_action( 'admin_head-edit.php', 'disable_welcome_to_the_block_editor' );
 
 }
