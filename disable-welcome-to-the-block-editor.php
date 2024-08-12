7cf59c9ef324c3cc1c0b573210f17786d8216fad
WPExplorer <wpexplorer@gmail.com>
2024-08-12 14:55:14 -0700

    Create disable-welcome-to-the-block-editor.php

---
+++ disable-welcome-to-the-block-editor.php
@@ -0,0 +1,22 @@
+/*
+ * Plugin Name: Disable "Welcome to the Block Editor"
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( is_admin() ) {
+
+  function disable_welcome_to_the_block_editor() {
+  	?>
+  	<script>
+  		window.onload = (event) => {
+  			wp.data && wp.data.select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) && wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
+  		};
+  	</script>
+  	<?php
+  }
+  add_action( 'admin_head-post.php', 'disable_welcome_to_the_block_editor' );
+  add_action( 'admin_head-post-new.php',  'disable_welcome_to_the_block_editor' );
+  add_action( 'admin_head-edit.php', 'disable_welcome_to_the_block_editor' );
+
+}
